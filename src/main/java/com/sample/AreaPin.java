ae9ff7a87d3dc5242fce293ecb917dbe34fb082c
Ramanpreet Khattra Kaur <ramanpreetkhattra@Sharaths-MacBook-Air.local>
2020-06-15 15:28:10 +0530

    initial commit

---
+++ src/main/java/com/sample/AreaPin.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class AreaPin {
+	private int code;
+	public AreaPin(int code) {
+		this.code = code;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+}
---
+++ src/main/java/com/sample/NumberFormatException.java
@@ -0,0 +1,8 @@
+package com.sample;
+
+public class NumberFormatException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public NumberFormatException(String msg) {
+		super(msg);
+	}
+}
